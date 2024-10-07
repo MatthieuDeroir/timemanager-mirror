6e1b9ff993993c5d4c248db53b6194823842d59f
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-10-07 17:08:22 +0200

    feat: :boom: Fixed migration order. Code Stabilized!

--- priv/repo/migrations/20241007142041_create_clocks.exs
+++ priv/repo/migrations/20241007142041_create_clocks.exs
@@ -7,6 +7,7 @@ defmodule TimeManagerApp.Repo.Migrations.CreateClocks do
       add :status, :boolean, default: false, null: false
       add :user_id, references(:users, on_delete: :nothing)
 
+
       timestamps(type: :utc_datetime)
     end
 
