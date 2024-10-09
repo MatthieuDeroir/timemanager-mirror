dbd4b6c0c9b9f8edd26473f9cbe4d19df7bec560
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-10-09 14:14:27 +0200

    feat: :sparkles:  added index to services

---
+++ frontend/src/services/index.js
@@ -0,0 +1,9 @@
+import clocks from './clocks_services';
+import users from './users_services';
+import workingtime from './workingtime_services';
+
+export const Services = {
+    Clocks: clocks,
+    Users: users,
+    WorkingTime: workingtime
+}
