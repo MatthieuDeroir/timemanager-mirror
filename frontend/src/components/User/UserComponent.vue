e0af337e5c31493123c742765e7067828b0d38c8
Terra <arthur.lefrancois@epitech.eu>
2024-10-09 16:41:15 +0200

    feat: :sparkles: add the frontend for user
    initialisation of the user on the frontend. No link with the DB for now

---
+++ frontend/src/components/User/UserComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="user-component">
+        <h2>{{ user.name }}</h2>
+        <p>Email: {{ user.mail }}</p>
+        <p>Age: {{ user.age }}</p>
+        <p>adresse : {{ user.address }}</p>
+        <p>adresse : {{ user.zipcode }}</p>
+    </div>
+
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    
+export default {
+    name: 'UserComponent',
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.user-component {
+    border: 1px solid #ccc;
+    padding: 16px;
+    border-radius: 8px;
+    max-width: 300px;
+    margin: 0 auto;
+}
+</style>
