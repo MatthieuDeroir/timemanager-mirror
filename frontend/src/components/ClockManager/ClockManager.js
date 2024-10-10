8e6c7652c477206aa327f22b1349552eb2155eed
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-10 20:18:02 +0200

    fix: :wrench: fix returned value for create clock endpoint

--- frontend/src/components/ClockManager/ClockManager.js
+++ frontend/src/components/ClockManager/ClockManager.js
@@ -1,4 +1,4 @@
-import Services from "@/services"
+import Services from '@/services'
 
 export default {
   data() {
@@ -10,13 +10,13 @@ export default {
     }
   },
   mounted() {
-  this.getData()
+    this.getData()
   },
   methods: {
     async getData() {
       try {
-        Services.Clocks.getClocksByUserId(1).then(data => {
-          this.items = data.filter(item => {
+        Services.Clocks.getClocksByUserId(1).then((data) => {
+          this.items = data.filter((item) => {
             const date = new Date(item.time);
             const dateNow = new Date();
             return (
@@ -24,38 +24,38 @@ export default {
               date.getMonth() === dateNow.getMonth() &&
               date.getDate() === dateNow.getDate()
             );
-          })
+          });
           if (this.items.length > 0) {
-            this.latestClockStatus = this.getLastClock(this.items);
-          } else { 
-            this.loading = false;
-            this.error = 'No clocks found for today';
+            this.latestClockStatus = this.getLastClock(this.items)
+          } else {
+            this.loading = false
+            this.error = 'No clocks found for today'
           }
-          this.loading = false;
+          this.loading = false
         })
-          if (this==null) {
-              this.loading = false;
-              this.error = 'No clocks found';
-          }
+        if (this == null) {
+          this.loading = false
+          this.error = 'No clocks found'
+        }
       } catch (err) {
-        this.error = err.message;
-        this.loading = false;
+        this.error = err.message
+        this.loading = false
       }
     },
 
     getLastClock(items) {
       return items.reduce((latest, item) => {
-        if (!item || !item.time || !latest || !latest.time)
-          return latest;
-        const currentClockDate = new Date(item.time);
-        const latestClockDate = new Date(latest.time);
-        return currentClockDate > latestClockDate ? item.status : latest.status;
-      },);
+        if (!item || !item.time || !latest || !latest.time) return latest
+        const currentClockDate = new Date(item.time)
+        const latestClockDate = new Date(latest.time)
+        return currentClockDate > latestClockDate ? item.status : latest.status
+      })
     },
 
-    async handleCreateClock(){
-      await Services.Clocks.createClock(new Date().toISOString(), !this.latestClockStatus, 1);
-      await this.getData();
+    async handleCreateClock() {
+      var mock = await Services.Clocks.createClock(new Date().toISOString(), !this.latestClockStatus, 1);
+      this.items.push(mock);
+      this.latestClockStatus = !this.latestClockStatus;
     }
   }
 }
