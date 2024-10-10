e68da2c8d7b7b94c52b243edd1eb043d27205fb8
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-10 20:24:34 +0200

    refactor: :recycle: refactor : main page is no more App.vue but Home.vue
    App.vue is only use for router and Home.vue use <main> to display our components

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,9 @@
 import { ref } from 'vue';
 import WorkingTimeManager from '@/components/WorkingTimeManager/WorkingTimeManager.vue';
 import WorkingTimeVisualization from '../components/WorkingTimeVisualization/WorkingTimeVisualization.vue';
+import Calendar from '../components/Calendar.vue'
+import ClockManager from '../components/ClockManager/ClockManager.vue'
+import UserComponent from '../components/User/UserComponent.vue';
 
 // Réactif pour stocker les données des working times
 const workingTimes = ref([]);
@@ -16,5 +19,8 @@ const updateWorkingTimes = (updatedTimes) => {
   <main>
     <WorkingTimeVisualization :userId="1" :workingTimes="workingTimes" />
     <WorkingTimeManager :userId="1" @workingTimesUpdated="updateWorkingTimes" />
+    <Calendar />
+    <ClockManager />
+    <UserComponent/>
   </main>
 </template>
