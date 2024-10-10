2cfbda5baa690bc6cd580d378ef33b014e9f91f8
ncesari <nicolas.cesari@epitech.eu>
2024-10-10 15:40:42 +0200

    feat: :sparkles: add calendar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Calendar from './components/Calendar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -15,13 +16,14 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
-
-  <RouterView />
+  </header> -->
+  <p>fff</p>
+  <Calendar />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -81,5 +83,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
