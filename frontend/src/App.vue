5b004d5f62a3f1fe91ad0928ba34c85861411cba
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-10 20:24:34 +0200

    refactor: :recycle: refactor : main page is no more App.vue but Home.vue
    App.vue is only use for router and Home.vue use <main> to display our components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,86 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Calendar from './components/Calendar.vue'
-import UserComponent from './components/User/UserComponent.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+   <header>
       <!-- The router-view will render the component matched by the route -->
       <router-view />
-      <Calendar />
-      <UserComponent />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
+      
   </header>
 </template>
 <style scoped>
-/* Your styles remain unchanged */
-
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 </style>
