2658bc3b56b84ec5f63e4ac9c109a49cf9da99b7
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 10:19:42 +0200

    feat: :tada: Header generated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 import Calendar from './components/Calendar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import UserComponent from './components/User/UserComponent.vue';
-
+import HeaderComponent from './components/Header/HeaderComponent.vue';
 </script>
 
 <template>
    <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <HeaderComponent />
     <div class="wrapper">
       <!-- The router-view will render the component matched by the route -->
       <router-view />
@@ -18,6 +18,7 @@ import UserComponent from './components/User/UserComponent.vue';
     </div>
   </header>
 </template>
+
 <style scoped>
 /* Your styles remain unchanged */
 
---
+++ frontend/src/components/Header/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <h1>My Simple Header</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script src="./HeaderComponent.js"></script>
+<style src="./HeaderComponent.css"></style>
