ea72879b8800dcf2d24691ab37af62d705b79f55
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 10:19:42 +0200

    feat: :tada: Header generated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
-import { RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from 'vue-router'
+import Calendar from './components/Calendar.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import UserComponent from './components/User/UserComponent.vue';
+import HeaderComponent from './components/Header/HeaderComponent.vue';
 </script>
 
 <template>
    <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <HeaderComponent />
+    <div class="wrapper">
       <!-- The router-view will render the component matched by the route -->
       <router-view />
       <Calendar />
       <UserComponent/>
   </header>
 </template>
+
 
 <style scoped>
 </style>
