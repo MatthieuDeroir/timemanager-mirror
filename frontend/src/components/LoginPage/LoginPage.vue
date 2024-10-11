6e46bfabf2d883101429cebf759c182f4d08d098
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 13:48:22 +0200

    feat: :sparkles: Login page fonction is up
    login page with anything in the mail or password pass the user to the main app

---
+++ frontend/src/components/LoginPage/LoginPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login-page">
+    <div class="login-container">
+      <img src="../../assets/Gotam-logo.png" alt="Gotham City Logo" class="logo" />
+      <h1>Login to Gotham</h1>
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" required />
+        </div>
+        <button type="submit" class="image-button">
+          <img src="../../assets/Batman.png" alt="Batman Logo" class="logo" />
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script src="./LoginPage.js"></script>
+<style src="./LoginPage.css"></style>
