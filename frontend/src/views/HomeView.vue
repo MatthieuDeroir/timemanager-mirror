da93f2fc967cde9014912e61c2c4f2e3ff3989ea
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 15:01:34 +0200

    feat: :fire: Re-add the login page into the project

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,7 +5,8 @@ import WorkingTimeVisualization from '../components/WorkingTimeVisualization/Wor
 import Calendar from '../components/Calendar.vue'
 import ClockManager from '../components/ClockManager/ClockManager.vue'
 import UserComponent from '../components/User/UserComponent.vue';
-
+import HeaderComponent from '../components/Header/HeaderComponent.vue';
+import LoginPage from '../components/LoginPage/LoginPage.vue';
 // Réactif pour stocker les données des working times
 const workingTimes = ref([]);
 
@@ -13,13 +14,33 @@ const workingTimes = ref([]);
 const updateWorkingTimes = (updatedTimes) => {
   workingTimes.value = updatedTimes;
 };
+
+// Authentication state
+const isAuthenticated = ref(false); 
+
+// Function to handle successful login
+function handleLoginSuccess() {
+  isAuthenticated.value = true;
+}
 </script>
 
 <template>
-  <main>
-    <WorkingTimeVisualization :userId="1" :workingTimes="workingTimes" />
-    <WorkingTimeManager :userId="1" @workingTimesUpdated="updateWorkingTimes" />
-    <Calendar />
-    <ClockManager />
-  </main>
+  <div>
+    <div v-if="!isAuthenticated">
+      <LoginPage @login-success="handleLoginSuccess" />
+    </div>
+
+    <div v-else>
+      <header>
+        <HeaderComponent />
+      </header>
+      <main>
+        <UserComponent/>
+        <WorkingTimeVisualization :userId="1" :workingTimes="workingTimes" />
+        <WorkingTimeManager :userId="1" @workingTimesUpdated="updateWorkingTimes" />
+        <Calendar />
+        <ClockManager />
+      </main>
+    </div>
+  </div>
 </template>
