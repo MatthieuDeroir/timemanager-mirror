213a82f8314d01c586e184b0784eb8a9951e83c2
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 14:21:25 +0200

    feat: :construction: commit before rebase

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import HeaderComponent from './components/Header/HeaderComponent.vue';
+import Calendar from './components/Calendar.vue';
+import UserComponent from './components/User/UserComponent.vue';
+
+  const isAuthenticated = ref(false); 
+
+  function handleLoginSuccess() {
+    isAuthenticated.value = true;
+  }
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+     <div v-if="!isAuthenticated">
+      <LoginPage @login-success="handleLoginSuccess" />
+    </div>
+
+    <div v-else>
+      <header>
+        <HeaderComponent />
+      </header>
+      <main>
+        <div class="wrapper">
+          <router-view />
+          <Calendar />
+          <UserComponent />
+        </div>  
+      </main>
+    </div>
 </template>
