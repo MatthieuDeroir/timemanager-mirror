b02b1d3124242b667e0069a4917f1cf805edce9e
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-11 16:29:45 +0200

    style: :lipstick: clock manager style

--- frontend/src/components/ClockManager/ClockManager.vue
+++ frontend/src/components/ClockManager/ClockManager.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>
-        <div v-if="latestClockStatus">green</div>
-        <div v-else-if="latestClockStatus == false">red</div>
-        <h1>ClockManager</h1>
+    <div class="clock-manager-container">
+        <div class="banner">
+            <h1>Today's clock in/out</h1>
+            <div class="clock-manager-header">
+                <h3>{{ this.date }}</h3>
+                <div class="clock-status" :class="currentClockStatus ? 'green' : 'red'"></div>
+            </div>
+        </div>
         <div>
             <div v-if="error">{{ this.error }}</div>
-            <div v-else-if="loading">Loading...</div>
+            <div v-else-if="loading">Chargement...</div>
             <div v-else>
-                <div>
-                    <li v-for="(item,index) in items" :key="index">
-                        {{ item.status }}  {{ item.user_id }} {{ item.time }}
-                    </li>
+                <div class="row-clock" v-for="(item,index) in items" :key="index">
+                    <div v-if="item.status">clock-out</div>
+                    <div v-else>clock-in</div>
+                    <div>
+                        {{ this.getTime(item) }}
+
+                    </div>
                 </div>
             </div>  
             <div>
@@ -20,3 +27,4 @@
     </div>
 </template>
 <script src="./ClockManager.js"/>
+<style src="./ClockManager.css"/>
