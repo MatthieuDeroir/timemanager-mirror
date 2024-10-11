d5e11898d1e3bffbc4365d1dbf17ba968c615806
Terra <arthur.lefrancois@epitech.eu>
2024-10-11 13:48:22 +0200

    feat: :sparkles: Login page fonction is up
    login page with anything in the mail or password pass the user to the main app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,4 @@
  
- 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -24,10 +23,32 @@ const vuetify = createVuetify({
   }
 })
  
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+ 
+/**
+* Create a new Vuetify instance
+*/
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+ 
 const app = createApp(App)
  
 app.use(vuetify)
-app.use(router)
  
+app.use(vuetify)
+app.use(router)
  
 app.mount('#app')
