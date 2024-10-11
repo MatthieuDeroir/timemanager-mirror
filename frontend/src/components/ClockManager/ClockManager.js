86aab364749f6592f14a271d422c78e6fe7ac501
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-11 10:25:59 +0200

    fix: :wrench: fix clocks status at page reload

--- frontend/src/components/ClockManager/ClockManager.js
+++ frontend/src/components/ClockManager/ClockManager.js
@@ -17,16 +17,18 @@ export default {
       try {
         Services.Clocks.getClocksByUserId(1).then((data) => {
           this.items = data.filter((item) => {
-            const date = new Date(item.time);
-            const dateNow = new Date();
+            const date = new Date(item.time)
+            const dateNow = new Date()
             return (
               date.getFullYear() === dateNow.getFullYear() &&
               date.getMonth() === dateNow.getMonth() &&
               date.getDate() === dateNow.getDate()
-            );
-          });
+            )
+          })
+          this.items.sort((latest, item) => new Date(latest.time) - new Date(item.time))
           if (this.items.length > 0) {
-            this.latestClockStatus = this.getLastClock(this.items)
+            console.log(this.getLastClock())
+            this.latestClockStatus = this.getLastClock();
           } else {
             this.loading = false
             this.error = 'No clocks found for today'
@@ -43,19 +45,19 @@ export default {
       }
     },
 
-    getLastClock(items) {
-      return items.reduce((latest, item) => {
-        if (!item || !item.time || !latest || !latest.time) return latest
-        const currentClockDate = new Date(item.time)
-        const latestClockDate = new Date(latest.time)
-        return currentClockDate > latestClockDate ? item.status : latest.status
-      })
+    getLastClock() {
+      return this.items[this.items.length -1].status
     },
 
     async handleCreateClock() {
-      var mock = await Services.Clocks.createClock(new Date().toISOString(), !this.latestClockStatus, 1);
-      this.items.push(mock);
-      this.latestClockStatus = !this.latestClockStatus;
+      var mock = await Services.Clocks.createClock(
+        new Date().toISOString(),
+        !this.latestClockStatus,
+        1
+      )
+      this.items.push(mock)
+      this.error = null
+      this.latestClockStatus = !this.latestClockStatus
     }
   }
 }
