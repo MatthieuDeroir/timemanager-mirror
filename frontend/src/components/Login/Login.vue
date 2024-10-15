4873e1d15bd3197dadd1251a26316ab4d5b507b6
Terra <arthur.lefrancois@epitech.eu>
2024-10-15 12:10:47 +0200

    feat: :lock: add the login screen, update CSS color to variable, change logo of gotham

--- frontend/src/components/Login/Login.vue
+++ frontend/src/components/Login/Login.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="login-page">
-    <div class="login-container">
-      <img src="../../assets/global/gotam-logo.png" alt="Gotham City Logo" class="logo" />
-      <h1>Login to Gotham</h1>
-      <form @submit.prevent="handleSubmit">
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input type="email" id="email" v-model="email" required />
-        </div>
-        <div class="form-group">
-          <label for="password">Password:</label>
-          <input type="password" id="password" v-model="password" required />
-        </div>
-        <button type="submit" class="image-button">
-          <img src="../../assets/global/batman.png" alt="Batman Logo" class="logo" />
-        </button>
-      </form>
+  <div class="background blue"></div>
+  <div class="login-wrapper">
+    <div class="login-page"> 
+      <div class="login-container">
+        <img src="../../assets/global/gotam-logo.png" alt="Gotham City Logo" class="logo" />
+        <h1>Login to Gotham</h1>
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required placeholder="Your email here" class="textcenter"/>
+          </div>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required placeholder="Your password here" class="textcenter"/>
+          </div>
+          <div class="white-circle">
+            <button type="submit" class="image-button">
+              <img src="../../assets/global/batman.png" alt="Batman Logo" class="logo" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
-<script src="Login.js"></script>
-<style src="Login.css"></style>
+<script src="./Login.js"></script>
+<style src="./Login.css"></style>
