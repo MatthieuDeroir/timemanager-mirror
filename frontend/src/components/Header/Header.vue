2d44d207d75454ed1b1cc9890ced4c17721601b8
Terra <arthur.lefrancois@epitech.eu>
2024-10-15 15:31:06 +0200

    feat: :tada: add the button to select a color

--- frontend/src/components/Header/Header.vue
+++ frontend/src/components/Header/Header.vue
@@ -3,13 +3,19 @@
     <img alt="Gotham Logo" class="navbar-logo" src="@/assets/global/batman.png"/>
     <h1>GOTHAM Time Manager</h1>
     <UserSearch @user-selected="handleUserSelected"/>
+    
     <div>
       <v-menu open-on-hover>
         <template v-slot:activator="{ props }">
           <v-btn color="primary" v-bind="props"> Duckler McQuack</v-btn>
         </template>
         <v-list>
-          <!-- Boucle pour afficher les options du menu -->
+          <!-- Add color picker to the dropdown menu -->
+          <v-list-item>
+            <v-list-item-title>
+              <ColorPicker />
+            </v-list-item-title>
+          </v-list-item>
           <v-list-item v-for="(item, index) in items" :key="index">
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
@@ -20,12 +26,13 @@
 </template>
 
 <script lang="ts" setup>
-import UserSearch from "@components/user/UserSearch/UserSearch.vue";
-import {ref} from 'vue';
+import UserSearch from "../../components/user/UserSearch/UserSearch.vue";
+import ColorPicker from "./ColorPicker/ColorPicker.vue";
+import { ref } from 'vue';
 
 const items = [
-  {title: 'Profil'},
-  {title: 'Log out'},
+  { title: 'Profil' },
+  { title: 'Log out' },
 ];
 
 const selectedUserId = ref<number | null>(null);
