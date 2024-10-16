168b0f9d73b3a78ef97d99dd70eff46cc064d1ef
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-16 16:53:41 +0200

    style: :lipstick: header new style without autocomplete style

--- frontend/src/components/Header/ColourPicker/ColourPicker.vue
+++ frontend/src/components/Header/ColourPicker/ColourPicker.vue
@@ -12,13 +12,13 @@
           @click="selectColor(color)"
           class="item"
         >          
-          <div class="circle-colour" :style="{ backgroundColor: color.colors[1] }"></div>
+          <div class="circle-colour" :style="{ background: 'var(--linear-'+color.name+')' }"></div>
         </v-list-item>
       </v-list>
     </v-menu>
   </div>
 </template>
-
+<!-- color.colors[1] -->
 <script lang="ts" setup>
 import { defineEmits, ref } from 'vue';
 
@@ -27,18 +27,18 @@ const selectedColor = ref(null);
 const emit = defineEmits(['colorSelected']);
 
 const colors = ref([
-  { name: 'Blue', colors: ['#5ba0ff', '#1a8eff', '#2d7eeb'] },
-  { name: 'Yellow', colors: ['#e6a536', '#e6bd1c', '#e18f02'] },
-  { name: 'Purple', colors: ['#8461c7', '#4b2aa9', '#5d35b7'] },
-  { name: 'Orange', colors: ['#fb7539', '#e47414', '#d16300'] },
-  { name: 'Green', colors: ['#429a42', '#48b942', '#139c1c'] },
-  { name: 'Red', colors: ['#e43732', '#c32929', '#db5a5a'] }
+  { name: 'blue' },
+  { name: 'yellow' },
+  { name: 'purple' },
+  { name: 'orange' },
+  { name: 'green' },
+  { name: 'red' }
 ]);
 
 const location = ref('start');
 
 const selectColor = (color) => {
-  selectedColor.value = color; 
+  selectedColor.value = color;
   localStorage.setItem('selectedColor', JSON.stringify(color)); 
   emit('colorSelected', color); 
 };
