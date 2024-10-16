84f31d5b540125e02cc681e0012fb5d61c3f6a22
Terra <arthur.lefrancois@epitech.eu>
2024-10-16 13:40:08 +0200

    feat: :fire: update menu and style for selecting color

--- frontend/src/components/Header/ColourPicker/ColourPicker.vue
+++ frontend/src/components/Header/ColourPicker/ColourPicker.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="text-center">
-    <v-menu>
+    <v-menu :location="location">
       <template v-slot:activator="{ props }">
-        <v-btn v-bind="props">Color Options</v-btn>
+        <v-btn v-bind="props">Colour selector</v-btn>
       </template>
 
-      <v-list>
+      <v-list class="list-items">
         <v-list-item
           v-for="(color, index) in colors"
           :key="index"
           @click="selectColor(color)"
-        >
-          <v-list-item-title>{{ color.name }}</v-list-item-title>
+          class="item"
+        >          
+          <div class="circle-colour" :style="{ backgroundColor: color.colors[1] }"></div>
         </v-list-item>
       </v-list>
     </v-menu>
-
-    <div class="selected-colour-info" v-if="selectedColor">
-      <ColourDisplayer :color="selectedColor" />
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import ColourDisplayer from './ColourDisplayer/ColourDisplayer.vue';
 import { defineEmits, ref } from 'vue';
 
 const selectedColor = ref(null);
@@ -31,21 +27,21 @@ const selectedColor = ref(null);
 const emit = defineEmits(['colorSelected']);
 
 const colors = ref([
-  { name: 'Blue' },
-  { name: 'Yellow' },
-  { name: 'Purple' },
-  { name: 'Orange' },
-  { name: 'Green' },
-  { name: 'Red' }
+  { name: 'Blue', colors: ['#5ba0ff', '#1a8eff', '#2d7eeb'] },
+  { name: 'Yellow', colors: ['#e6a536', '#e6bd1c', '#e18f02'] },
+  { name: 'Purple', colors: ['#8461c7', '#4b2aa9', '#5d35b7'] },
+  { name: 'Orange', colors: ['#fb7539', '#e47414', '#d16300'] },
+  { name: 'Green', colors: ['#429a42', '#48b942', '#139c1c'] },
+  { name: 'Red', colors: ['#e43732', '#c32929', '#db5a5a'] }
 ]);
+
+const location = ref('start');
 
 const selectColor = (color) => {
   selectedColor.value = color; 
   localStorage.setItem('selectedColor', JSON.stringify(color)); 
   emit('colorSelected', color); 
 };
-
-
 </script>
 
 <style scoped src="./ColourPicker.css"></style>
