bc111c9797c657ad3738105e0b20dd31d35c1af2
AlexandreLefay <alexandre.lefay@gmail.com>
2024-10-17 19:30:55 +0200

    feat: :sparkles: Add ClockStore/ClockAPI & refacto clock component

---
+++ frontend/src/api/ClockAPI.js
@@ -0,0 +1,30 @@
+import {HttpClient} from '@/api/config/HttpClient.js'
+
+/**
+ * getClocksByUserId
+ * This method fetches clocks for a specific user by their ID.
+ * @param {number} userId
+ * @returns {Promise} - Returns a promise with the response data.
+ */
+export const getClocksByUserId = (userId) =>
+  HttpClient.get(`/clocks/${userId}`).then((res) => res.data)
+
+/**
+ * createClock
+ * This method creates a new clock for a specific user.
+ * @param {Object} data - The clock data (time, status, userId).
+ * @returns {Promise} - Returns a promise with the response data.
+ */
+export const createClock = (data) =>
+    HttpClient.post(`/clocks/${data.userId}`, {
+      clocks: {
+        time: data.time,
+        status: data.status,
+        userId: data.userId
+      }
+    }).then((res) => res.data)
+
+export default {
+  getClocksByUserId,
+  createClock
+}
