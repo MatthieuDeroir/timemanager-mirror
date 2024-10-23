6d174804f69b2eab91c5daaa1a13602ebef0e623
Benjamin Couet <benjamin.couet@epitech.eu>
2024-10-23 19:40:51 +0200

    feat: :art: UI for user add and delete in team component

--- frontend/src/api/TeamAPI.js
+++ frontend/src/api/TeamAPI.js
@@ -37,7 +37,7 @@ export const getUsersFromTeamId = (teamId) =>
 
 
 export const addUserInTeam = (teamId, userId) =>
-  HttpClient.post(`/user_teams/add`)
+  HttpClient.post(`/user_teams/add`,{userId, teamId})
     .then((res) => res.data)
     .catch((error) => {
       console.error('Error adding a new user in team:', error)
@@ -46,7 +46,7 @@ export const addUserInTeam = (teamId, userId) =>
 
 
 export const deleteUserInTeam = (teamId, userId) =>
-  HttpClient.delete(`/user_teams/delete`)
+  HttpClient.post(`/user_teams/delete`,{userId, teamId})
     .then((res) => res.data)
     .catch((error) => {
       console.error('Error deleting a user in team:', error)
--- frontend/src/components/Team/Team.vue
+++ frontend/src/components/Team/Team.vue
@@ -5,7 +5,10 @@
     <h3 v-if="teams.length<=0" class="no-team">You are not affected to any teams</h3>
     <v-expansion-panel  v-for="team in teams" :key="team.id">
       <v-expansion-panel-title>
-        <h3>{{ team.name }} </h3>
+        <div class="team-expansion-panel">
+          <h3>{{ team.name }} </h3>
+          <svg v-if="authStore.user.role_id != UserRole.EMPLOYEE" class="add-user-in-team" @click="handleAddUserInTeam" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>minus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+        </div>
       </v-expansion-panel-title>
       <v-expansion-panel-text v-if="team.users">
         <v-table density="compact" fixed-header="true">
@@ -28,6 +31,13 @@
                 <a :href="`mailto:${user.email}`" class="mailto-link">
                   <img :src="emailIcon" alt="Email Us" />
                 </a>
+              </td>
+              <td v-if="authStore.user.role_id != UserRole.EMPLOYEE">
+                <svg @click="handleDeleteUserFromTeam" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ff0000">
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                  <g id="SVGRepo_iconCarrier"> <path d="M15 12H9" stroke="#d11a1a" stroke-width="1.5" stroke-linecap="round"/> <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#d11a1a" stroke-width="1.5" stroke-linecap="round"/> </g>
+                </svg>
               </td>
             </tr>
           </tbody>
@@ -74,6 +84,13 @@ const handleCreateTeam = (event) => {
   if (teamName != ''){
     teamStore.createTeam(teamName)
   }
+};
+
+const handleAddUserInTeam = () => {
+  //TODO
+};
+const handleDeleteUserFromTeam = () => {
+  //TODO
 };
 
 onMounted(() => {
