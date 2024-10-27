6c343c7c9b94e34164ab0d71ba50279698f6b177
Terra <arthur.lefrancois@epitech.eu>
2024-10-27 20:58:31 +0100

    fix: :bug: add path to config in vite.config.phone

--- frontend/vite.config.phone.js
+++ frontend/vite.config.phone.js
@@ -16,6 +16,7 @@ export default defineConfig({
       '@api': fileURLToPath(new URL('./src/services', import.meta.url)),
       '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
       '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
       '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
       '@enum': fileURLToPath(new URL('./src/enum', import.meta.url)),
       '@styles': fileURLToPath(new URL('./src/styles', import.meta.url))
